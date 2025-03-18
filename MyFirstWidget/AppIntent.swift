abd060d88ecce411ab6ff40f5ae799398c1707f6
shaun <doit_2017@outlook.com>
2025-03-18 17:22:25 +0800

    初始化项目

---
+++ MyFirstWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  MyFirstWidget
+//
+//  Created by smartPayt on 2025/3/18.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ MyFirstWidget/MyFirstWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstWidgetBundle.swift
+//  MyFirstWidget
+//
+//  Created by smartPayt on 2025/3/18.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MyFirstWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MyFirstWidget()
+        MyFirstWidgetControl()
+    }
+}
