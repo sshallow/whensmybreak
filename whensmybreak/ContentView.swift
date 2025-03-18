8480326d8cbf4a1c82aa73105a8592f14063580c
shaun <doit_2017@outlook.com>
2025-03-18 14:09:45 +0800

    Initial Commit

---
+++ whensmybreak/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  whensmybreak
+//
+//  Created by smartPayt on 2025/3/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ whensmybreak/whensmybreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  whensmybreakApp.swift
+//  whensmybreak
+//
+//  Created by smartPayt on 2025/3/18.
+//
+
+import SwiftUI
+
+@main
+struct whensmybreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
